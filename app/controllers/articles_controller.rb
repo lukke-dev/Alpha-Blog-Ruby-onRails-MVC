985ff14620f03cbbc88acbb202355455ed1698a8
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-20 12:13:26 -0300

    redirect new to index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,6 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     @article.save
-    redirect_to article_path(@article)
+    redirect_to @article
   end
 end
