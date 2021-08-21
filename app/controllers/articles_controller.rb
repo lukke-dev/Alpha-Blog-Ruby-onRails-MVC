968bb8e875a079299870b8752ffe09b1b6504372
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-21 01:01:01 -0300

    add flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,11 +7,17 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def new; end
+  def new
+    @article = Article.new
+  end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = 'Artigo criado com sucesso'
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 end
