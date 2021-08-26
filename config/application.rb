0b5cacb0e6aa77e0ed02edba0064359291a9ab3d
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-26 08:34:21 -0300

    add pt-BR.yml file

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +11,7 @@ module AlphaBlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.default_locale = :"pt-BR"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
