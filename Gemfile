612495305b89f3c3a479ad9ba91361f3afdcfed2
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-27 20:18:55 -0300

    add bcrypt, has_secure_password

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'webpacker', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'bootstrap', '~> 5.1.0'
+gem 'bcrypt', '~> 3.1.7'
 
 
 gem 'bootsnap', '>= 1.4.4', require: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_221929) do
+ActiveRecord::Schema.define(version: 2021_08_27_231349) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_08_27_221929) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
