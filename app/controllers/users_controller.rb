ecf2ad37e959b20f8930a94ce6201c376f761d62
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-28 03:34:21 -0300

    add users create action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(article_params)
+    if @user.save
+      flash[:notice] = "Bem vindo #{@user.username} ao Blog Alpha, usuário criado com sucesso"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
 
   resources :articles
+  resources :users, except: [:new]
 end
