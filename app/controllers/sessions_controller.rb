d396a05dcee7380135d487901fb81f3f5578e532
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-29 13:16:48 -0300

    add button back-to-top

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,15 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    byebug
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user_id
+      flash[:notice] = 'logado com sucesso'
+      redirect_to user_path(user.id)
+    else
+      flash.now[:alert] = 'Email ou Senha inválido'
+      render 'new'
+    end
   end
 
   def destroy; end
