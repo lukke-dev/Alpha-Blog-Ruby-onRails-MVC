11ba03529bca56adc7cb87638e679eb90509e7b7
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-29 13:32:03 -0300

    add button login and logout / add session new-create-destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user_id
-      flash[:notice] = 'logado com sucesso'
+      session[:user_id] = user.id
+      flash[:notice] = 'Logado com sucesso'
       redirect_to user_path(user.id)
     else
       flash.now[:alert] = 'Email ou Senha inválido'
@@ -13,5 +13,9 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Usuário desconectado'
+    redirect_to root_path
+  end
 end
