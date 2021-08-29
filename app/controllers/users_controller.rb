9018d2bb4e7695c5d12b3906f46937ac73978a68
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-28 21:46:55 -0300

    add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update show]
+
   def new
     @user = User.new
   end
@@ -13,12 +15,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(article_params)
       flash[:notice] = 'Usuário atualizado com sucesso!'
       redirect_to articles_path
@@ -27,9 +26,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @articles = @user.articles
+  end
+
   private
 
   def article_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
