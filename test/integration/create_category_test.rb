7a8785eb9be7c73ba6fe36b65dd54bb15f60e415
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-31 04:35:25 -0300

    add integration test to test for invalid submissions

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,15 +1,14 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
-  test 'get new category form adn create category' do
+  test 'get new category form and reject invalida category submission' do
     get '/categories/new'
     assert_response :success
-    assert_difference 'Category.count', 1 do
-      post categories_path, params: { category: { name: 'Esportes' } }
-      assert_response :redirect
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: ' ' } }
     end
-    follow_redirect!
-    assert_response :success
-    assert_match 'Esportes', response.body
+    assert_match 'erros', response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
   end
 end
