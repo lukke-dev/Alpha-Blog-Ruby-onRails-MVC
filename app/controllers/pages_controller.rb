3b54102d9695192f48e62639c560e82bfc80f633
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-02 20:13:22 -0300

    add search by name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,9 @@ class PagesController < ApplicationController
   end
 
   def about; end
+
+  def search
+    @query = params[:title]
+    @articles = Article.where('title like ?', "%#{@query}%").order(:title).page(params[:page])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  get 'search', to: 'pages#search', as: :search
 
   resources :articles
   resources :users
