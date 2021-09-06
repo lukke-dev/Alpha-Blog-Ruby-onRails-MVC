2b0bce14af4d3145b50d9a06ace8eaea35e10da1
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-06 12:48:30 -0300

    completed categories show and index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.page params[:page]
   end
 
   def new
